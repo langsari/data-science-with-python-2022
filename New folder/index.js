bba7e26e22721cae9226b13bacdbaa12dc31ed0b
Yusrqnz <konmanloo2701@gmail.com>
2023-02-17 06:08:10 +0700

    Weather App

---
+++ New folder/index.js
@@ -0,0 +1,10 @@
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-bot button');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details')
+const error404 = document.querySelector('.not-found');
+
+search.addEventListener('click',()=>{
+
+
+})
